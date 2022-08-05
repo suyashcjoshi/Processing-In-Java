0b06829c514a25a9c28f07ec4d0b5fbdfd2f2fbb
Suyash Joshi <suyashjoshi@users.noreply.github.com>
2022-08-05 03:04:32 -0700

    Updated with comments and correct formatting

--- src/main/java/dev/suyashjoshi/creativecode/App.java
+++ src/main/java/dev/suyashjoshi/creativecode/App.java
@@ -10,26 +10,27 @@ package dev.suyashjoshi.creativecode;
 import processing.core.PApplet;
 
 public class App extends PApplet{
-	
-	public void settings(){
-		size(500, 500);
-	}
-	
-	public void draw(){
-		ellipse(mouseX, mouseY, 50, 50);
-	}
-	
-	public void mousePressed(){
-		background(64);
-	}
-	
-	public static void main(String[] args){
+
+    // Main entry point to the program that needs to start the PApplet
+    public static void main(String[] args){
         try {
-            PApplet.main("dev.suyashjoshi.creativecode.App");
-        }
+                PApplet.main("dev.suyashjoshi.creativecode.App");
+            }
         catch (Exception e) {
-            e.printStackTrace(new java.io.PrintStream(System.out));
-        }
-        
-	}
+                e.printStackTrace(new java.io.PrintStream(System.out));
+        }    
+    }
+
+    // Processing sketch helper methods	
+    public void settings(){
+        size(500, 500);
+    }
+	
+    public void draw(){
+        ellipse(mouseX, mouseY, 50, 50);
+    }
+	
+    public void mousePressed(){
+        background(64);
+    }
 }
